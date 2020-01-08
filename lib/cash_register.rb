42bb6b18739ab9d5a55aa73d28e5f6fd4bc4efea
Kate Raskauskas <katherine.raskauskas@gmail.com>
2020-01-08 09:24:48 -0500

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,46 @@
+require 'pry'
 
+
+class CashRegister
+    attr_reader :discount
+    attr_accessor :total
+    attr_accessor :items
+
+
+    def initialize(employee_discount=nil)
+        @total = 0
+        @discount = employee_discount
+        if @discount
+            @discount_message = "After the discount, the total comes to $"
+        else
+            @discount_message = "There is no discount to apply."
+        end
+        @items = []
+        @items_prices = []
+        @items_nums = []
+    end
+
+    def add_item(title,price,num=1)
+        @total += price * num
+        @items += [title] * num
+        @items_prices += [price] * num
+        @items_nums << num
+    end
+
+    def apply_discount
+        if @discount
+            @total -= @total * @discount/100
+            @discount_message += "#{@total}."
+        end
+        @discount_message
+    end
+
+    def void_last_transaction
+        @num = @items_nums.pop
+        @items = @items[0..-(@num + 1)]
+        @total -= @items_prices[-@num..-1].sum
+        @items_prices = @items_prices[0..-(@num+1)]
+    end
+
+
+end
